276bcaf84177e47c2010693913a48854d696fe42
liufan262 <liufan262@gmail.com>
2018-07-17 22:24:43 +0800

    login

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ if(!port){
 
 var server = http.createServer(function(request, response){
   var parsedUrl = url.parse(request.url, true)
-  var pathWithQuery = request.url 
+  var pathWithQuery = request.url
   var queryString = ''
   if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
   var path = parsedUrl.pathname
@@ -27,25 +27,80 @@ var server = http.createServer(function(request, response){
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.end(string)
   }else if(path === '/signUp' && method === 'POST'){
-  getPostData(request, function (postData) {
-      let {email,password,password_confirmation} = postData
-      let errors = {}
-      // check email
-      if(email.indexOf('@') <= 0){
-        errors.email = '邮箱不合法'
-      }
-      if(password.length < 6){
-        errors.password = '密码太短'
-      }
-      if(password_confirmation !== password){
-        errors.password_confirmation = '两次输入密码不匹配'
-      }
+      getPostData(request, function (postData) {
+          let {email,password,password_confirmation} = postData
+          let errors = {}
+          // check email
+          if(email.indexOf('@') <= 0){
+            errors.email = '邮箱不合法'
+          }
+          if(password.length < 6){
+            errors.password = '密码太短'
+          }
+          if(password_confirmation !== password){
+            errors.password_confirmation = '两次输入密码不匹配'
+          }
 
+          if(Object.keys(errors).length === 0){
+             let {email, password} = postData
+              let user = {
+                 email: email,
+                  passwordHash: liufanHas(password)
+              }
+              //写数据库
+              let dbString = fs.readFileSync('./db.json','utf-8')
+              let dbObject = JSON.parse(dbString)
+              dbObject.users.push(user)
+              let dbString2 = JSON.stringify(dbObject)
+              fs.writeFileSync('./db.json',dbString2,{encoding:'utf-8'})
+
+          }else{
+              response.statusCode = 400
+          }
+
+          response.setHeader('Content-Type', 'text/html;charset=utf-8')
+          response.end(JSON.stringify(errors))
+      })
+  }else if(path === '/node_modules/jquery/dist/jquery.min.js'){
+      let string = fs.readFileSync('./node_modules/jquery/dist/jquery.min.js')
+      response.statusCode = 200
+      response.setHeader('Content-Type', 'application/javascript;charset=utf-8')
+      response.end(string)
+  }else if(path === '/main.js'){
+      let string = fs.readFileSync('./main.js')
+      response.statusCode = 200
+      response.setHeader('Content-Type', 'application/javascript;charset=utf-8')
+      response.end(string)
+  }else if(path === '/home'){
+      let string = fs.readFileSync('./home')
+      response.statusCode = 200
       response.setHeader('Content-Type', 'text/html;charset=utf-8')
-      response.end(JSON.stringify(errors))
-  })
-  }
-  else{
+      response.end(string)
+  }else if(path === '/login' && method === 'POST' ){
+      let string = fs.readFileSync('./home')
+      response.statusCode = 200
+      //读数据库
+      getPostData(request,(postData)=>{
+          let dbString = fs.readFileSync('./db.json','utf-8')
+          let dbObject = JSON.parse(dbString)
+          let users = dbObject.users
+
+          let{email,password} = postData
+          let found
+          for(var i=0;i<dbObject.users.length; i++){
+              if (users[i].emaill === email && users[i].passwordHash === liufanHas(password)){
+                found = users[i]
+                  break
+              }
+          }
+          if(found){
+              //标记该用户登录了
+              response.setHeader('Set-Cookie', {logined: true})
+          }
+      })
+
+      response.end(string)
+  }else{
     response.statusCode = 404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write('呜呜呜')
@@ -66,12 +121,41 @@ function getPostData(request, callback) {
         let postData = {}
         for(var i=0; i<array.length; i++){
             let parts = array[i].split('=')
-            let key =parts[0]
-            let value = parts[1]
+            let key = decodeURIComponent(parts[0])
+            let value = decodeURIComponent(parts[1])
             postData[key] = value
         }
         callback.call(null , postData)
     })
+}
+
+function liufanHas(string) {
+    return 'liufan' + string + 'liufan'
+}
+
+function parseCookies(cookie) { // JSON.parse
+    try{
+        return cookie.split(';').reduce(
+            function(prev, curr) {
+                var m = / *([^=]+)=(.*)/.exec(curr);
+                var key = m[1];
+                var value = decodeURIComponent(m[2]);
+                prev[key] = value;
+                return prev;
+            },
+            { }
+        );
+    }catch(error){
+        return {}
+    }
+}
+
+function stringifyCookies(cookies) { //JSON.stringify
+    var list = [ ];
+    for (var key in cookies) {
+        list.push(key + '=' + encodeURIComponent(cookies[key]));
+    }
+    return list.join('; ');
 }
 
 server.listen(port)
